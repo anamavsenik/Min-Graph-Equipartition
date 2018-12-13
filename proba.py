e4bcb6d14714950bb5e28067b1dfdabb0a6a742e
anamkravanja <anamarija.kravanja@gmail.com>
2018-12-13 21:40:48 +0100

    ...

---
+++ proba.py
@@ -0,0 +1,15 @@
+# libraries
+import matplotlib.pyplot as plt
+import networkx as nx
+import pandas as pd
+
+# Build a dataframe with 4 connections
+df = pd.DataFrame({'from': ['A', 'B', 'C', 'A'], 'to': ['D', 'A', 'E', 'C']})
+df
+
+# Build your graph
+G = nx.from_pandas_dataframe(df, 'from', 'to')
+
+# Plot it
+nx.draw(G, with_labels=True)
+plt.show()
