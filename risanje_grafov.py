0a2b912176d156c36aebe58890334371cbb1a31d
anamkravanja <anamarija.kravanja@gmail.com>
2018-12-17 11:10:20 +0100

    risanje grafov sedaj deluje

--- risanje_grafov.py
+++ risanje_grafov.py
@@ -1,19 +1,16 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import numpy
 
-A=numpy.matrix([[0,0,1,0,1,0,0],[0,0,1,1,0,0,0],[1,1,0,0,0,1,0],[0,1,0,0,1,0,1],[1,0,0,1,0,1,1],[0,0,1,0,1,0,1],[0,0,0,1,1,1,0]])
-G=nx.from_numpy_matrix(A)
-nx.draw_networkx(G)
-plt.show()
-
-#predloga barvanja grafov na dve barvi:
-G = nx.erdos_renyi_graph(20,0.1)
-color_map = []
-for node in G:
-    if node <10:
-        color_map.append('blue')
-    else:
-        color_map.append('green')
-nx.draw(G,node_color = color_map,with_labels = True)
-plt.show()
+def pobarvaj_graf(A,X,Y):  #iz matrike A nariše graf v dveh barvah
+    M=numpy.matrix(A)
+    G=nx.from_numpy_matrix(M)
+    color_map = []
+    for node in G:
+        if node in X:
+            color_map.append('blue')
+        else:
+            color_map.append('green')
+    nx.draw(G,node_color = color_map,with_labels = True)
+    plt.show()
 
